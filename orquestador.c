18d1935cc4b112afdd6de4689a3c7f00791bd244
Jonatan <jonatan.verab@gmail.com>
2026-04-11 16:52:47 -0300

    Creacion de los procesos hijos

--- orquestador.c
+++ orquestador.c
@@ -18,5 +18,34 @@ int main(int argc, char *argv[]){
         printf("Error: El valor de N debe ser un numero positivo.\n");
         return 1;
     }
+    // Registro de la función de finalización
+        atexit(mensaje_final);
+        
+        pid_t pids[n];
+        
+        for(int i=0;i<n;i++){
+            // Creación de procesos hijos
+            pids[i]= fork();
+            
+            if(pids[i]<0){
+                perror("Error al crear el proceso hijo");
+                exit(1);
+            }
+            
+            if(pids[i]==0){
+                // Generación de semilla para números aleatorios
+                srand(getpid());
+                int tiempo = (rand() % 5) + 1;
+                
+                printf("[HIJO %d] Mi PID es %d y el de mi padre es %d. Dormire %d segundos.\n",i+1,getpid(),getppid(),tiempo);
+                
+                sleep(tiempo);
+                
+                printf("[HIJO %d] Termine mi tarea.\n",i+1);
+                
+                // Finalización del proceso hijo
+                _exit(i+1);
+            }
+        }
     
    
