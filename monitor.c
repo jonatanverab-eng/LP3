53966b071314d006758184b852df0b518d93ed58
jbrondell <jackelinegimenez3@gmail.com>
2026-04-12 06:23:47 +0000

    manejo básico de SIGINT y SIGTERM

---
+++ monitor.c
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+// Contador global  
+volatile sig_atomic_t contador_global= 0;
+
+// Manejador de señales(2)
+void manejador(int senhal) {
+    if (senhal == SIGINT) {
+        printf("\nRecibido SIGINT (Ctrl+C), pero NO termino.\n");
+    } else if (senhal == SIGTERM) {
+        printf("\nRecibido SIGTERM. Liberando recursos y terminando...\n");
+        exit(0);
+    }
+}
+
+int main() {
+
+//Definir la estructura de la configuracion de mi señal(1)
+    struct sigaction signhal;
+
+   // pasar la funcion que maneja las señales al sa_handler
+    signhal.sa_handler = manejador;
+  // sigemptyset no bloquear ninguna señal extra dentro el handler
+    sigemptyset(&signhal.sa_mask);
+  //ningun comportamiento extra
+    signhal.sa_flags = 0;
+
+
+//asocia la accion a la señal. Pasa la estructura de configuracion
+    sigaction(SIGINT, &signhal, NULL);
+    sigaction(SIGTERM, &signhal, NULL);
+
+    // Esperar señales(4)
+    printf("Esperando señales...\n");
+
+    while (1) {
+        pause(); // espera hasta recibir una señal
+    }
+
+    return 0;
+}
