11a8f22a171c09c7beb5a1b5674a4531b5d1510a
Diego Candia <142847942+DiegoCR16@users.noreply.github.com>
2026-04-13 17:29:28 -0300

    Refactor signal handler and improve argument validation

--- parcial1.c
+++ parcial1.c
@@ -4,20 +4,28 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+// Handler de señales
 void handler(int sig) {
     if (sig == SIGUSR1) {
-        printf("Soy PID %d y recibi SIGUSR1\n", getpid());
+        printf("Soy %d y recibi SIGUSR1\n", getpid());
     } else if (sig == SIGTERM) {
-        printf("Soy PID %d y finalizo\n", getpid());
+        printf("Soy %d y finaliza\n", getpid());
         exit(0);
     }
 }
 
-int main() {
-    pid_t pid;
-    struct sigaction sa;
+int main(int argc, char *argv[]) {
 
-    // Configurar handler
+    // Validar argumento
+    if (argc != 2) {
+        printf("Uso: %s <cantidad_hijos>\n", argv[0]);
+        exit(1);
+    }
+
+    int N = atoi(argv[1]);
+    pid_t *hijos = malloc(N * sizeof(pid_t));
+
+    struct sigaction sa;
     sa.sa_handler = handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
@@ -25,35 +33,39 @@ int main() {
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    pid = fork();
+    // Crear hijos
+    for (int i = 0; i < N; i++) {
+        hijos[i] = fork();
 
-    if (pid < 0) {
-        perror("Error en fork");
-        exit(1);
-    }
+        if (hijos[i] == 0) {
+            printf("Hijo listo. PID: %d\n", getpid());
 
-    if (pid == 0) {
-        // HIJO
-        printf("Hijo ejecutandose con PID %d\n", getpid());
-        printf("Esperando señales...\n");
-
-        while (1) {
-            pause(); // espera señales indefinidamente
+            while (1) {
+                pause();
+            }
         }
-
-    } else {
-        // PADRE
-        printf("Padre PID %d, hijo PID %d\n", getpid(), pid);
-
-        // El padre solo monitorea
-        waitpid(pid, NULL, 0);
-
-        printf("Hijo finalizado\n");
     }
 
-    return 0;
-}
+    // Padre
+    printf("\n=== PADRE ===\n");
+    printf("Cantidad de hijos: %d\n", N);
 
-    printf("PADRE: Todos los procesos hijos han terminado. Cerrando supervisor.\n");
+    for (int i = 0; i < N; i++) {
+        printf("PID hijo %d: %d\n", i, hijos[i]);
+    }
+
+    printf("\nUsa otra terminal para enviar señales:\n");
+    printf("kill -SIGUSR1 <PID>\n");
+    printf("kill -SIGTERM <PID>\n\n");
+
+    // Esperar hijos
+    for (int i = 0; i < N; i++) {
+        waitpid(hijos[i], NULL, 0);
+        printf("Hijo %d terminó\n", hijos[i]);
+    }
+
+    free(hijos);
+
+    printf("Todos los hijos finalizaron\n");
     return 0;
 }
